04dcc51205c8fa2733a606c8654c7dea47da7f5d
KNikhith <nikhithkotluri@gmail.com>
2026-04-21 16:26:39 +0530

    Done

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,14 +2,11 @@ import React, { useState } from "react";
 
 export default function Tooltip({text,children}){
     let [show,setShow]=useState(false);
-    const handleShow=()=>{
-        setShow(prev=>!prev);
-    }
     return(
         <div 
         className="tooltip"
-        onMouseEnter={handleShow}
-        onMouseLeave={handleShow}
+        onMouseEnter={()=>setShow(true)}
+        onMouseLeave={()=>setShow(false)}
         >
             {children}
             {show && (<span className="tooltiptext">{text}</span>)}
